65744e83f1d05904af0f3373c62444de5ab4420f
Amber <martinez.amber1028@gmail.com>
2022-11-13 23:17:22 -0800

    commit tree f77bfdae940612739b6d1f3d778548138b10bc50

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -3,13 +3,29 @@ class CheesesController < ApplicationController
   # GET /cheeses
   def index
     cheeses = Cheese.all
-    render json: cheeses
+
+    if cheeses
+      render json: cheeses, except: [:created_at, :updated_at], methods: [:summary]
+    else 
+      render json: { error: 'Cheese not found' }, status: :not_found
+    end
+
   end
 
   # GET /cheeses/:id
   def show
     cheese = Cheese.find_by(id: params[:id])
-    render json: cheese
+
+    if cheese
+    render json: {
+      id: cheese.id,
+      name: cheese.name,
+      price: cheese.price,
+      is_best_seller: cheese.is_best_seller
+    }
+    else 
+      render json: { error: 'Cheese not found' }
+    end
   end
 
 end
